946811ca3d8612c7e83f2b834abb8db293e80bd9
Carlijn van der Weerd <c.van.der.weerd@student.rug.nl>
2024-09-17 10:36:06 +0200

    update lecture

--- r-scripts/08_regression_modelling.R
+++ r-scripts/08_regression_modelling.R
@@ -151,7 +151,7 @@ p1
 m5<-glm(CountSum~elevation_m,
         family=poisson(log),
         data=orchdat3)
-anova(m5,"Chisq")
+anova(m5,test="Chisq")
 
 orchdat3$pred5<-predict(m5,type="response")
 p1 + geom_line(data=orchdat3, aes(y=pred5), linewidth=1.2)
@@ -176,7 +176,23 @@ p1 + geom_line(data=orchdat3, aes(y=pred6), linewidth=1.2)
 
 # add the interaction to the model: elevation + elevation ^2 + year + elevation*year
 # now test and show  the effect of both elevation + year
+anova(m7,m6,test="Chisq")
 
+m7<-glm(CountSum~elevation_m+I(elevation_m^2) + factor(year),
+       family=poisson(log),
+       data=orchdat3)
+orchdat3$pred7<-predict(m7,type="response")
+p1 + geom_line(data=orchdat3, aes(y=pred7), linewidth=1.2)
+
+m8<-glm(CountSum~elevation_m+I(elevation_m^2) + factor(year) + elevation_m*factor(year),
+        family=poisson(log),
+        data=orchdat3)
+orchdat3$pred8<-predict(m8,type="response")
+p1 + geom_line(data=orchdat3, aes(y=pred8), linewidth=1.2)
+
+anova(m8,m7,test="Chisq")
+
+## m7 is better than m8! Interaction between year*elevation_m is important in this case. In the polynomial model the effect of elevation was not significant anymore
 
 #add the  model to the plot
 # calculate the predicted value of m2 for every observation, add to the dataset as a variable as pred2
